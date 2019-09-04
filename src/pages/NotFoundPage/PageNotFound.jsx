fa72fe4f7dd18f0c6e3e31cf63cf8c8c0e200fd6
elenems <mickhaelyarish@gmail.com>
2019-09-04 13:36:29 +0300

    Added page not found

--- src/pages/NotFoundPage/PageNotFound.jsx
+++ src/pages/NotFoundPage/PageNotFound.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import "./PageNotFound.css";
+import { Link } from "react-router-dom";
 export default function PageNotFound() {
-    return (
-        <div>
-        </div>
-    )
+  return (
+    <div id="pageNotFound">
+      <div>
+        <h1>Page not found</h1>
+        <Link to="/">Return home</Link>
+      </div>
+    </div>
+  );
 }
